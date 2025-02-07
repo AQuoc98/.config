051dd18303de40befa649804846e7cad58b7a905
Quốc Anh <quocanh@192.168.2.204.non-exists.ptr.local>
2025-02-08 02:08:37 +0700

    feat(nvim): add lazygit, comment plugin

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,4 +1,27 @@
 return {
+  {
+    "folke/ts-comments.nvim",
+    enabled = false,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    opts = {},
+    config = function()
+      local comment = require("Comment")
+      local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+
+      comment.setup({
+        -- for commenting tsx, jsx, svelte, html files
+        pre_hook = ts_context_commentstring.create_pre_hook(),
+      })
+    end,
+  },
+
   -- Create annotations with one keybind, and jump your cursor in the inserted annotation
   {
     "danymat/neogen",
