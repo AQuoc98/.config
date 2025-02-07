8b377c6d8760be748c5718e08f9a768efae39d9f
Quốc Anh <quocanh@192.168.2.204.non-exists.ptr.local>
2025-02-08 04:28:08 +0700

    feat(nvim): add multiple cursor plugin like vscode

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -260,4 +260,22 @@ return {
       vim.g.lazygit_config_file_path = {} -- table of custom config file paths
     end,
   },
+
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
 }
