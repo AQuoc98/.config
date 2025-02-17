2353c7d5fec4b5da50c7979174b3fe0ad8d55542
Quốc Anh <quocanh@192.168.2.3.non-exists.ptr.local>
2025-02-17 16:33:11 +0700

    feat(nvim): disable go to location lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -150,91 +150,91 @@ return {
       setup = {},
     },
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
-      local function go_to_location(method)
-        local params = vim.lsp.util.make_position_params()
-        vim.lsp.buf_request(0, method, params, function(err, result)
-          if err then
-            vim.notify("LSP Error: " .. err.message, vim.log.levels.ERROR)
-            return
-          end
-
-          if not result or vim.tbl_isempty(result) then
-            vim.notify("No results found", vim.log.levels.WARN)
-            return
-          end
-
-          -- Ensure result is alway list
-          if not vim.tbl_islist(result) then
-            result = { result }
-          end
-
-          if #result > 1 then
-            require("telescope.builtin").lsp_definitions()
-            return
-          end
-
-          local location = result[1]
-          local uri = location.uri or location.targetUri
-          if not uri then
-            vim.notify("Invalid location result", vim.log.levels.ERROR)
-            return
-          end
-
-          local target_path = vim.uri_to_fname(uri)
-          local current_path = vim.api.nvim_buf_get_name(0)
-
-          -- Nếu file khác, mở tab mới
-          if target_path ~= current_path then
-            vim.cmd("tabnew " .. vim.fn.fnameescape(target_path))
-          end
-
-          -- Nhảy đến vị trí cụ thể trong file
-          local range = location.range or location.targetSelectionRange
-          local row = range.start.line + 1
-          local col = range.start.character + 1
-          vim.api.nvim_win_set_cursor(0, { row, col })
-        end)
-      end
-
-      vim.list_extend(keys, {
-        {
-          "gd",
-          function()
-            go_to_location("textDocument/definition")
-          end,
-          desc = "Go to definition",
-          has = "definition",
-        },
-        {
-          "gD",
-          function()
-            go_to_location("textDocument/declaration")
-          end,
-          desc = "Go to declaration",
-          has = "declaration",
-        },
-        {
-          "gI",
-          function()
-            go_to_location("textDocument/implementation")
-          end,
-          desc = "Go to implementation",
-          has = "implementation",
-        },
-        {
-          "gy",
-          function()
-            go_to_location("textDocument/typeDefinition")
-          end,
-          desc = "Goto type definition",
-          has = "typeDefinition",
-        },
-      })
-    end,
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function()
+  --     local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  --
+  --     local function go_to_location(method)
+  --       local params = vim.lsp.util.make_position_params()
+  --       vim.lsp.buf_request(0, method, params, function(err, result)
+  --         if err then
+  --           vim.notify("LSP Error: " .. err.message, vim.log.levels.ERROR)
+  --           return
+  --         end
+  --
+  --         if not result or vim.tbl_isempty(result) then
+  --           vim.notify("No results found", vim.log.levels.WARN)
+  --           return
+  --         end
+  --
+  --         -- Ensure result is alway list
+  --         if not vim.tbl_islist(result) then
+  --           result = { result }
+  --         end
+  --
+  --         if #result > 1 then
+  --           require("telescope.builtin").lsp_definitions()
+  --           return
+  --         end
+  --
+  --         local location = result[1]
+  --         local uri = location.uri or location.targetUri
+  --         if not uri then
+  --           vim.notify("Invalid location result", vim.log.levels.ERROR)
+  --           return
+  --         end
+  --
+  --         local target_path = vim.uri_to_fname(uri)
+  --         local current_path = vim.api.nvim_buf_get_name(0)
+  --
+  --         -- Nếu file khác, mở tab mới
+  --         if target_path ~= current_path then
+  --           vim.cmd("tabnew " .. vim.fn.fnameescape(target_path))
+  --         end
+  --
+  --         -- Nhảy đến vị trí cụ thể trong file
+  --         local range = location.range or location.targetSelectionRange
+  --         local row = range.start.line + 1
+  --         local col = range.start.character + 1
+  --         vim.api.nvim_win_set_cursor(0, { row, col })
+  --       end)
+  --     end
+  --
+  --     vim.list_extend(keys, {
+  --       {
+  --         "gd",
+  --         function()
+  --           go_to_location("textDocument/definition")
+  --         end,
+  --         desc = "Go to definition",
+  --         has = "definition",
+  --       },
+  --       {
+  --         "gD",
+  --         function()
+  --           go_to_location("textDocument/declaration")
+  --         end,
+  --         desc = "Go to declaration",
+  --         has = "declaration",
+  --       },
+  --       {
+  --         "gI",
+  --         function()
+  --           go_to_location("textDocument/implementation")
+  --         end,
+  --         desc = "Go to implementation",
+  --         has = "implementation",
+  --       },
+  --       {
+  --         "gy",
+  --         function()
+  --           go_to_location("textDocument/typeDefinition")
+  --         end,
+  --         desc = "Goto type definition",
+  --         has = "typeDefinition",
+  --       },
+  --     })
+  --   end,
+  -- },
 }
