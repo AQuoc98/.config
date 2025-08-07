824a8a8333f70c170f5a3d0386af328e3791e36c
Ken <ken@Kens-MacBook-Pro.local>
2025-08-07 21:29:55 +0700

    feat: update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,31 +1,4 @@
-local wezterm = require 'wezterm'
-
-local config = wezterm.config_builder()
-
--- Font settings
-config.font_size = 16
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
-
--- Colors
-config.colors = {
-cursor_bg = "white",
-cursor_border = "white"
-}
-
--- Appearance
-config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
-config.window_padding = {
-left = 0,
-right = 0,
-top = 0,
-bottom = 0
-}
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
-
--- Miscellaneous settings
-config.max_fps = 120
-config.prefer_egl = true 
+local config = require("config")
+require("events")
 
 return config
